329958855e9f4286eafb40c41029c00de1b94e51
HeshanKarunaratne <heshan947102@gmail.com>
2022-12-21 11:54:52 +0530

    Added Bootstrap

--- src/app/courses.components.ts
+++ src/app/courses.components.ts
@@ -3,15 +3,8 @@ import { Component } from "@angular/core"
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{title}}</h2>
-        <table>
-            <tr>
-                <td [attr.colspan]="colspan"></td>
-            </tr>
-        </table>
+            <button class="btn btn-primary">Save</button>
         `
 })
 export class CoursesComponent {
-    title = "List of courses";
-    colspan = 2;
 }
