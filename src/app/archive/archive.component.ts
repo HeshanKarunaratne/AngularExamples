bbf049489b3b028b549de88d7b9e5d03b5a0f8ba
HeshanKarunaratne <heshan947102@gmail.com>
2023-02-07 23:43:44 +0530

    routing exercise complete

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -10,18 +10,14 @@ export class ArchiveComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) { }
   year: any;
   month: any;
-  ngOnInit(): void {
-    let year = this.route.snapshot.paramMap.get('year');
-    let month = this.route.snapshot.paramMap.get('month');
 
-    this.year = year;
-    this.month = month;
-    console.log("year: " + year);
-    console.log("month: " + month);
+  ngOnInit(): void {
+    this.year = this.route.snapshot.paramMap.get('year');
+    this.month = this.route.snapshot.paramMap.get('month');
   }
 
   goBack() {
-    this.router.navigate(['']);
+    this.router.navigate(['/']);
   }
 
 }
