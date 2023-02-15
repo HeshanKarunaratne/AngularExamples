99dc7a9a77b22cca60860644876a105f97de0515
HeshanKarunaratne <heshan947102@gmail.com>
2023-02-15 21:52:22 +0530

    environments

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  navBarBackgroundColor = environment.navBarBackgroundColor;
   constructor() { }
 
   ngOnInit() {
+    console.log("Background color is: " + this.navBarBackgroundColor);
   }
 
 }
